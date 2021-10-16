ae8593b16003cf4a2db37a9b24d0bcab54213e60
koolgax99 <nihar.sanda@gmail.com>
2021-10-16 16:45:35 +0530

    testing deployment

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require('path');
 const cors = require("cors");
 const db = require("./app/models");
@@ -7,16 +6,14 @@ const db = require("./app/models");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:3000"
+    origin: "*"
 };
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //mongodb connection 
 db.mongoose
@@ -36,20 +33,16 @@ db.mongoose
 //importing all the routes
 require("./app/routes/contacts.routes")(app);
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-//     app.get('*', (req, res) => {
-//         res.sendfile(
-//             path.join(__dirname = 'client/build/index.html')
-//         );
-//     }
-//     )
-// }
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build'))
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendfile(
+            path.join(__dirname = 'client/build/index.html')
+        );
+    }
+    )
+}
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
